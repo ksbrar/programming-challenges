d822257f244e6a9ddfeb61f36dc9698ce89ba3bf
ksbrar <kabir@brar.xyz>
2019-06-27 15:49:36 -0700

    Add solution for wp 249.

--- interview-cake/weekly-problem-249/solution.py
+++ interview-cake/weekly-problem-249/solution.py
@@ -4,8 +4,48 @@ class LinkedListNode(object):
         self.value = value
         self.next  = None
 
+'''
+Both methods below are O(n), though the first requires two walks through the
+list of nodes (my solution) and the second uses two pointers to perform only
+one walk (second of two solutions provided by Interview Cake, more clever IMO).
+'''
 def kth_to_last_node(k, node):
-  pass
+  # Count list length:
+  list_length = 0
+  n = node
+  while (n != None):
+    n = n.next
+    list_length += 1
+  
+  # Break if list too short:
+  if list_length < k or k == 0:
+    print('k out of bounds!')
+    return
+  
+  # Otherwise find the (len-k)th node:
+  kn = node
+  for _ in range(list_length-k):
+    kn = kn.next
+  print('Kth node: ' + kn.value)
+
+def kth_to_last_node_2(k, node):
+  # Initialize walkers:
+  ln = node
+  rn = node
+  if (k == 0):
+    print('k out of bounds!')
+    return 
+
+  for _ in range(k):
+    if (rn == None):
+      print('k out of bounds!')
+      return
+    rn = rn.next
+  
+  while (rn != None):
+    ln = ln.next
+    rn = rn.next
+  print('Kth node: ' + ln.value)
 
 def main():
   a = LinkedListNode('Angel Food')
@@ -20,6 +60,7 @@ def main():
   d.next = e
 
   kth_to_last_node(2, a)
+  kth_to_last_node_2(2, a)
 
 if __name__ == "__main__":
   main()
